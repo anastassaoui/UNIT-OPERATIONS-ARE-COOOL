66569bbf4f12ffaa5e0ec7bb7b5aa8398df10e28
anastassaoui <148813612+anastassaoui@users.noreply.github.com>
2024-10-08 19:44:35 +0100

    added stuff

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ def get_url(url: str):
         return None
     return r.json()
 
-    
+url1 = get_url("https://lottie.host/b1031118-99fc-47da-8103-0d692d09c1cd/6Myzvt3heg.json")
+url2 = get_url("https://lottie.host/c21a9cff-c3f0-4cca-b335-6133fbad9533/U1NqieB2ON.json")
+url3 = get_url("https://lottie.host/33f398ca-7a6d-4434-97f8-4e5dbb34a625/ytJiSFcQ3s.json")  
 
 
 st.sidebar.title("Navigation",)
@@ -51,29 +53,69 @@ if page == "Home":
     <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700 md:-mt-7">
     """, unsafe_allow_html=True)
 
-    
-    url1 = get_url("https://lottie.host/b1031118-99fc-47da-8103-0d692d09c1cd/6Myzvt3heg.json")
+
 
     col1 , col2 = st.columns([2,1])
     with col1:
         st.markdown("""
-        <h1 class=" text-md  font-extrabold cursor-pointer capitalize hover:text-red-400 duration-1000 md:text-5xl md:mt-28
-                ">
+
+        <h4 class="text-xl font-bold mt-5 hover:text-red-400 duration-1000 cursor-pointer hover:underline md:-mb-12 md:mt-20 md:text-6xl">
+            Total Attendance Sessions 
+        </h4>
+
+        <p class="mb-3 text-lg  md:text-xl text-white md:mt-20 cursor-pointer">
+            Welcome to our project for chemical engineering students! This tool lets you explore and visualize key unit operations.
+        </p>
+        <p class=" text-white cursor-pointer">
+            With engaging simulations, you can deepen your understanding of essential topics such as mass transfer and thermodynamics, providing a hands-on way to apply your knowledge.
+        </p>
+
+        """, unsafe_allow_html=True)
+        
+    with col2:
+        st_lottie(url1, width=500, height=500)
+
+    
+   
+    
+    col1 , col2 = st.columns([1,2])
+    with col1:
+        st_lottie(url2, width=300, height=300)
+    with col2:
+                st.markdown("""
+
+    <h4 class="text-xl font-bold mt-5 hover:text-red-400 duration-1000 cursor-pointer hover:underline md:-mb-12 md:mt-20 md:text-6xl">
+        Total Attendance Sessions 
+    </h4>
+
+    <p class="mb-3 text-lg  md:text-xl text-white md:mt-20 cursor-pointer">
         Welcome to our project for chemical engineering students! This tool lets you explore and visualize key unit operations.
-        </h1>
-
-        <h1 class=" text-md  font-extrabold cursor-pointer capitalize hover:text-red-400 duration-1000 md:text-5xl 
-                ">
+    </p>
+    <p class=" text-white cursor-pointer">
         With engaging simulations, you can deepen your understanding of essential topics such as mass transfer and thermodynamics, providing a hands-on way to apply your knowledge.
-        </h1>
-
-
+    </p>
     
     """, unsafe_allow_html=True)
-    with col2:
-        st_lottie(url1, width=600, height=600)
-
     
+    
+    col1 , col2 = st.columns([2,1])
+    with col2:
+        st_lottie(url3, width=400, height=400)
+    with col1:
+                st.markdown("""
+
+    <h4 class="text-xl font-bold mt-5 hover:text-red-400 duration-1000 cursor-pointer hover:underline md:-mb-12 md:mt-40 md:text-6xl">
+        Total Attendance Sessions 
+    </h4>
+
+    <p class="mb-3 text-lg  md:text-xl text-white md:mt-20 cursor-pointer">
+        Welcome to our project for chemical engineering students! This tool lets you explore and visualize key unit operations.
+    </p>
+    <p class=" text-white cursor-pointer">
+        With engaging simulations, you can deepen your understanding of essential topics such as mass transfer and thermodynamics, providing a hands-on way to apply your knowledge.
+    </p>
+    
+    """, unsafe_allow_html=True)
     
     
     
