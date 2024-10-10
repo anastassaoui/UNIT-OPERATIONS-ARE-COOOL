62fc49b6fcc360185a37a24a5b2872fe763b54cb
anastassaoui <148813612+anastassaoui@users.noreply.github.com>
2024-10-10 20:29:32 +0100

    finished kinetics

--- app1.py
+++ app1.py
@@ -17,11 +17,13 @@ def get_url(url: str):
 
 url1 = get_url("https://lottie.host/01168c02-1c9e-493e-85f2-343c86c43950/fkBauqcPOH.json")
 url2 = get_url("https://lottie.host/c8ce4ada-d2f8-42f8-ab21-689abf4e9aae/3yeact3Hyb.json")
+url3 = get_url("https://lottie.host/ec9a9ff6-561d-40c4-9d73-1d10cadb1773/IsBsAaSi64.json")
+url4 = get_url("https://lottie.host/d4cbdfc3-631d-4868-a579-414c0f7c15ba/aTaSBPnHZE.json")
+url5 = get_url("https://lottie.host/ba08f5b8-8d62-4c39-9cd9-80b3b7c2ed13/EYPiFLUScU.json")
 
 
 def display1():
-    
-    ###################################tailwind stuff##############################################
+
     tailwind_cdn = """
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
@@ -33,11 +35,7 @@ def display1():
         </style>
     """
     st.markdown(tailwind_cdn, unsafe_allow_html=True)
-    
-    ##############################################################################################
-    
-    
-    #############################################Title###########################################
+
     col1,col2,col3 = st.columns([1,3,1])
     with col1:
         st_lottie(url1, width=300, height=300)
@@ -59,7 +57,6 @@ def display1():
         """, unsafe_allow_html=True)
     with col3:
         st_lottie(url1, width=300, height=300)
-    ########################################################################################
     
     
 
@@ -99,55 +96,67 @@ def display1():
     with col2:
         st_lottie(url2, width=400, height=400)
         
-    st.latex(r"""
-        \textbf{Second-Order Reactions}
-        """)
-    st.latex(r"""
-        \text{Rate law:}
-        """)
-    st.latex(r"""
-        \text{Rate} = -\frac{d[A]}{dt} = k[A]^2
-        """)
-    st.latex(r"""
-        \text{In a second-order reaction, the rate is proportional to the square of the concentration of the reactant.}
-        """)
-    st.latex(r"""
-        \text{Differential equation:}
-        """)
-    st.latex(r"""
-        \frac{d[A]}{dt} = -k[A]^2
-        """)
-    st.latex(r"""
-        \text{Solution:}
-        """)
-    st.latex(r"""
-        [A](t) = \frac{[A]_0}{1 + k[A]_0 t}
-        """)
+        
+        
+        
+    col1,col2 = st.columns([1,2])
+    with col1:
+        st_lottie(url3, width=350, height=400)
+    
+    
+    with col2:
+        st.latex(r"""
+            \textbf{Second-Order Reactions}
+            """)
+        st.latex(r"""
+            \text{Rate} = -\frac{d[A]}{dt} = k[A]^2
+            """)
+        st.latex(r"""
+            \text{The rate is proportional to the square of the concentration of the reactant.}
+            """)
+        st.latex(r"""
+            \text{Differential equation:}
+            """)
+        st.latex(r"""
+            \frac{d[A]}{dt} = -k[A]^2
+            """)
+        st.latex(r"""
+            \text{Solution:}
+            """)
+        st.latex(r"""
+            [A](t) = \frac{[A]_0}{1 + k[A]_0 t}
+            """)
+    col1,col2 = st.columns([2,1])
+    
+    with col1:
+        st.latex(r"""
+            \textbf{Third-Order Reactions}
+            """)
+        st.latex(r"""
+            \text{Rate} = -\frac{d[A]}{dt} = k[A]^3
+            """)
+        st.latex(r"""
+            \text{The rate is proportional to the cube of the concentration of the reactant.}
+            """)
+        st.latex(r"""
+            \text{Differential equation:}
+            """)
+        st.latex(r"""
+            \frac{d[A]}{dt} = -k[A]^3
+            """)
+        st.latex(r"""
+            \text{Solution:}
+            """)
+        st.latex(r"""
+            [A](t) = \frac{[A]_0}{(1 + 2k[A]_0^2 t)^{1/2}}
+            """)
+    with col2:
+        st_lottie(url4, width=400, height=450)
 
-    st.latex(r"""
-        \textbf{Third-Order Reactions}
-        """)
-    st.latex(r"""
-        \text{Rate law:}
-        """)
-    st.latex(r"""
-        \text{Rate} = -\frac{d[A]}{dt} = k[A]^3
-        """)
-    st.latex(r"""
-        \text{The rate of a third-order reaction is proportional to the cube of the concentration of the reactant.}
-        """)
-    st.latex(r"""
-        \text{Differential equation:}
-        """)
-    st.latex(r"""
-        \frac{d[A]}{dt} = -k[A]^3
-        """)
-    st.latex(r"""
-        \text{Solution:}
-        """)
-    st.latex(r"""
-        [A](t) = \frac{[A]_0}{(1 + 2k[A]_0^2 t)^{1/2}}
-        """)
+    ##😊😘😂😊💕😁😁👌💕😁❤️👌😁🤦‍♂️😎😉🤷‍♂️😊😘😂😊💕😁😁👌💕😁❤️👌😁🤦‍♂️😎😉🤷‍♂️
+    ##😊😘😂😊💕😁😁👌💕😁❤️👌😁🤦‍♂️😎😉🤷‍♂️😊😘😂😊💕😁😁👌💕😁❤️👌😁🤦‍♂️😎😉🤷‍♂️
+    ##😊😘😂😊💕😁😁👌💕😁❤️👌😁🤦‍♂️😎😉🤷‍♂️😊😘😂😊💕😁😁👌💕😁❤️👌😁🤦‍♂️😎😉🤷‍♂️
+    ##😊😘😂😊💕😁😁👌💕😁❤️👌😁🤦‍♂️😎😉🤷‍♂️😊😘😂😊💕😁😁👌💕😁❤️👌😁🤦‍♂️😎😉🤷‍♂️
 
     def first_order(C, t, k):
         return -k * C
@@ -167,12 +176,12 @@ def display1():
     )
 
     k = st.sidebar.slider("Rate Constant (k)", min_value=0.0, max_value=20.0, value=1.0)
-    C0 = st.sidebar.slider("Initial Concentration (C₀)", min_value=0.0, max_value=20.0, value=1.0)
+    C0 = st.sidebar.slider("Initial Concentration (C₀)", min_value=0.001, max_value=50.0, value=1.0)
     time_max = st.sidebar.slider("Maximum Time", min_value=1, max_value=100, value=50)
 
-    t = np.linspace(0, time_max, 500)
+    t = np.linspace(0, time_max, 400)
 
-    # Add progress bar and status text
+    #progress bar
     progress_bar = st.sidebar.progress(0)
     status_text = st.sidebar.empty()
 
@@ -184,25 +193,24 @@ def display1():
     elif reaction_order == "Third Order":
         C = odeint(third_order, C0, t, args=(k,))
 
-    # Update progress bar to indicate completion
+    # Update progress bar
     for i in range(1, 101):
-        time.sleep(0.01)  # Simulate progress delay
+        time.sleep(0.1)  #progress delay
         progress_bar.progress(i)
         status_text.text(f"{i}% Complete")
 
-    # Clear the progress bar after computation is done
     progress_bar.empty()
 
 
 
-    col1, col2, col3 = st.columns([1, 1, 1])
+    col1, col2 = st.columns([1, 1])
     with col1:
         # Plot the data
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=t, y=C[:, 0], mode='lines', name='Concentration'))
 
         fig.update_layout(
-            title='Concentration vs. Time',
+            title='Concentration vs Time',
             xaxis_title='Time',
             yaxis_title='Concentration',
             template='plotly_dark'
@@ -210,8 +218,11 @@ def display1():
 
         st.plotly_chart(fig)
 
-    # Calculate and compare different initial concentrations
     C0_doubled = 2 * C0
+    c0_tripplled =3 * C0
+    c0_quad =4 * C0
+    c0_cinq =5 * C0    
+    
 
     if reaction_order == "First Order":
         C_doubled = odeint(first_order, C0_doubled, t, args=(k,))
@@ -219,11 +230,46 @@ def display1():
         C_doubled = odeint(second_order, C0_doubled, t, args=(k,))
     elif reaction_order == "Third Order":
         C_doubled = odeint(third_order, C0_doubled, t, args=(k,))
-    
+        
+    if reaction_order == "First Order":
+        C_trip = odeint(first_order, c0_tripplled, t, args=(k,))
+    elif reaction_order == "Second Order":
+        C_trip = odeint(second_order, c0_tripplled, t, args=(k,))
+    elif reaction_order == "Third Order":
+        C_trip = odeint(third_order, c0_tripplled, t, args=(k,))
+        
+    if reaction_order == "First Order":
+        C_quad = odeint(first_order, c0_quad, t, args=(k,))
+    elif reaction_order == "Second Order":
+        C_quad = odeint(second_order, c0_quad, t, args=(k,))
+    elif reaction_order == "Third Order":
+        C_quad = odeint(third_order, c0_quad, t, args=(k,))
+        
+    if reaction_order == "First Order":
+        C_quad = odeint(first_order, c0_quad, t, args=(k,))
+    elif reaction_order == "Second Order":
+        C_quad = odeint(second_order, c0_quad, t, args=(k,))
+    elif reaction_order == "Third Order":
+        C_quad = odeint(third_order, c0_quad, t, args=(k,))
+
+    if reaction_order == "First Order":
+        C_cinq = odeint(first_order, c0_cinq, t, args=(k,))
+    elif reaction_order == "Second Order":
+        C_cinq = odeint(second_order, c0_cinq, t, args=(k,))
+    elif reaction_order == "Third Order":
+        C_cinq = odeint(third_order, c0_cinq, t, args=(k,))
+        
+        
+        
+            
     with col2:
         fig2 = go.Figure()
         fig2.add_trace(go.Scatter(x=t, y=C[:, 0], mode='lines', name=f'C₀ = {C0}'))
         fig2.add_trace(go.Scatter(x=t, y=C_doubled[:, 0], mode='lines', name=f'C₀ = {C0_doubled}'))
+        fig2.add_trace(go.Scatter(x=t, y=C_trip[:, 0], mode='lines', name=f'C₀ = {c0_tripplled}'))
+        fig2.add_trace(go.Scatter(x=t, y=C_quad[:, 0], mode='lines', name=f'C₀ = {c0_quad}'))
+        fig2.add_trace(go.Scatter(x=t, y=C_cinq[:, 0], mode='lines', name=f'C₀ = {c0_cinq}'))
+
 
         fig2.update_layout(
             title='Effect of Doubling Initial Concentration',
@@ -236,44 +282,52 @@ def display1():
 
     rate_of_change = -k * C[:, 0] if reaction_order == "First Order" else -k * C[:, 0]**(2 if reaction_order == "Second Order" else 3)
     half_life = np.log(2) / k if reaction_order == "First Order" else 1 / (k * C0) if reaction_order == "Second Order" else None
-    with col3:
+
+        
+
+
+    col1, col2 = st.columns([4,2])  
+    with col1:
+        data = pd.DataFrame({
+            'Time': t,
+            'Concentration': C[:, 0],
+            'Rate of Change': rate_of_change,
+            'Normalized Concentration': C[:, 0] / C0,
+            'Cumulative Change': np.cumsum(np.diff(C[:, 0], prepend=C0)),
+            'Half-Life (if applicable)': half_life if half_life else 'N/A'
+        })
+        st.write("### Concentration Data Over Time", data)
+    with col2:
+        st_lottie(url5, width=400, height=450)
+        
+        
+    col1,col2 = st.columns([2,2])
+    with col1:
         fig3 = go.Figure()
         fig3.add_trace(go.Scatter(x=t, y=rate_of_change, mode='lines', name='Rate of Change'))
-
         fig3.update_layout(
             title='Rate of Change of Concentration vs. Time',
             xaxis_title='Time',
             yaxis_title='Rate of Change',
             template='plotly_dark'
         )
-
         st.plotly_chart(fig3)
-        # Create a DataFrame to display the data
-        
 
-
-    col1, col2 = st.columns([2,2])  
-    with col1:
-        
-        data = pd.DataFrame({
-            'Time': t,
-            'Concentration': C[:, 0],
-            'Rate of Change': rate_of_change,
-            'Half-Life (if applicable)': half_life if half_life else 'N/A'
-        })
-        st.write("### Concentration Data Over Time", data)
-
-        
-           
     with col2:
-        
-        rate_data = pd.DataFrame({
-            'Time': t,
-            'Rate of Change': rate_of_change
-        })
-        st.write("### Rate of Change", rate_data)
-    
-    
+        fig6 = go.Figure()
+
+        fig6.add_trace(go.Scatter(x=C[:, 0], y=rate_of_change, mode='lines', name='Phase Plot'))
+
+        fig6.update_layout(
+            title='Phase Plot: Concentration vs Rate of Change',
+            xaxis_title='Concentration',
+            yaxis_title='Rate of Change',
+            template='plotly_dark'
+        )
+
+        st.plotly_chart(fig6)
+
+
     
 if __name__ == "__main__":
     display1()
